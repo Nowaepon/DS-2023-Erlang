37f53f6db196d107cbd3fbf8f2a5193bf7c51120
Nowaepon <77643077+Nowaepon@users.noreply.github.com>
2024-01-15 17:59:07 +0000

    Lösung für Übung 11 überarbeitet

--- tutorial/excercise_11/bully.erl
+++ tutorial/excercise_11/bully.erl
@@ -2,42 +2,104 @@
 -compile().
 
 
+%Zum Senden der coordinator-Nachrichten an alle Prozesse außer sich selbst und den alten Coordinator
+sendNewCoordinator(NewCoordPID, OldCoordPID, []) -> ok;
+sendNewCoordinator(NewCoordPID, OldCoordPID, [LastPID]) -> 
+    case
+        CurrentPID == NewCoordPID -> ok;
+        CurrentPID == OldCoordPID -> ok;
+        true -> CurrentPID!{coordinator, NewCoordPID}
+    end.
+sendNewCoordinator(NewCoordPID, OldCoordPID, [FirstPID|OtherPIDs]) -> 
+    case
+        CurrentPID == NewCoordPID -> sendNewCoordinator(NewCoordPID, OldCoordPID, OtherPIDs);
+        CurrentPID == OldCoordPID -> sendNewCoordinator(NewCoordPID, OldCoordPID, OtherPIDs);
+        true -> CurrentPID!{coordinator, NewCoordPID}
+    end.
+
+
 %Zum Senden der Election-Nachrichten an alle höheren PIDs
-sendElections(ownerPID, []) -> ok.
-sendElections(ownerPID, [lastPID]) -> 
-    case ownerPID < lastPID of
-        true -> lastPID!election
-sendElections(ownerPID, [firstPID|otherPIDs]) ->
-    case ownerPID < firstPID of
-        true -> firstPID!election, sendElections(ownerPID, otherPIDs);
-        false -> sendElections(ownerPID, otherPIDs)
+sendElections(OwnerPID, []) -> ok.
+sendElections(OwnerPID, [LastPID]) -> 
+    case OwnerPID < LastPID of
+        true -> LastPID!{election, OwnerPID}
+sendElections(OwnerPID, [FirstPID|OtherPIDs]) ->
+    case OwnerPID < FirstPID of
+        true -> FirstPID!{election, OwnerPID}, sendElections(OwnerPID, OtherPIDs);
+        false -> sendElections(OwnerPID, OtherPIDs)
     end.
 
 % Zur Intitialisierung der Processes nötig
 bullyProcess(undefined, undefined) ->
     receive
-        {startup, coordPID, otherPIDs} -> bullyProcess(coordPID, otherPIDs)
+        {startup, CoordPID, OtherPIDs} -> bullyProcess(CoordPID, OtherPIDs)
+    end.
 
 %TODO: Noch unvollständig, ABlauflogik nicht klar
 %      Noch gar nicht beachtet ist, wenn man selbst election Nachrichten erhält, während man gerade auf z. B. den rpc wartet.
-bullyProcess(coordPID, otherPIDs) ->
+bullyProcess(CoordPID, OtherPIDs) ->
     if 
         %Bin ich selbst der Coordinator? Dann muss ich auf rpcs antworten
-        coordPID == self() ->
+        CoordPID == self() ->
             receive
-                {PID, Request} -> PID!response, bullyProcess(coordPID, otherPIDs)
+                {PID, request} -> PID!{self(), response}, bullyProcess(CoordPID, OtherPIDs)
+            % TODO: Irgendwie auf election eingehen?
             end
-        %Anernfalls überprüfe, ob der Coordinator erreichbar ist.
+        %Andernfalls überprüfe, ob der Coordinator erreichbar ist.
         %Wenn ja, alles gut, und wiederhole die Prüfung (Intervall oder so wegen Nachrichtenspam?).
         %Wenn nein, starte eine Election.
         true ->
-            if
-                rpc:rpc(coordPID, request) == response -> bullyProcess(coordPID, otherPIDs)
-                true -> 
-
+            CoordPID!{self(), request}
             receive
-                election -> ;
-                coordinator -> ;
+                {Pid , response} -> bullyProcess(CoordPID, OtherPIDs);
+                startElection -> beginElection(self(), CoordPID, OtherPIDs)
+            %TODO: Müssen wir election-Nachrichten von neuen prozessen beachten und in OtherPIDs einfügen?
+                {election, SenderPid} -> 
+                    SenderPid!ok,
+                    receive
+                        {coordinator, NewCoordPID} -> beginElection(self(), CoordPID, OtherPIDs)
+                    end
+            after 250
+                %Starte Election
+                beginElection(self(), CoordPID, OtherPIDs)
+
+                % sendElections(self(), OtherPIDs)
+
+                % receive
+                %     ok -> waitForNewCoordinator(OtherPIDs)
+                % after 250
+                %     %Dann bin ich selbst coordinator
+                %     sendNewCoordinator(self(), CoordPID, OtherPIDs);
+                % end
+            end
+    end.
+
+%Beginnt damit, eine Election auszuführen
+beginElection(OwnPID, OldCoordPID, OtherPIDs) ->
+    sendElections(OwnPID, OtherPIDs)
+
+    receive
+        ok -> waitForNewCoordinator(OtherPIDs)
+    after 250
+        %Dann bin ich selbst coordinator
+        sendNewCoordinator(self(), OldCoordPID, OtherPIDs);
+    end
+
+%Damit mehrere ok-Nachrichten auch alle aufgebraucht werden und nicht in der Messagebox verbleiben
+%Wie abzufangen, wenn letztes ok nach coordinator kommt?
+waitForNewCoordinator(OtherPIDs) ->
+    receive
+        {coordinator, NewCoordPID} -> bullyProcess(NewCoordPID, OtherPIDs),
+        ok -> waitForNewCoordinator(OtherPIDs);
+    .
+
+            %if
+            %    rpc:rpc(CoordPID, request) == response -> bullyProcess(CoordPID, OtherPIDs)
+            %    true -> 
+%
+            %receive
+            %    election -> ;
+            %    coordinator -> ;
     .
 
 %Aufgabenteil b)
@@ -53,11 +115,11 @@ setupIntern(Amount, ListPIDs) ->
 
 %Starte die Logik aller Prozesse, indem ihnen der Initial-Koordinator und die anderen Prozesse bekannt gemacht werden.
 %setupInternStartup(coordPID, allPIDs, []) -> darfNichtVorkommen.
-setupInternStartup(coordPID, allPIDs, []) -> allPIDs.
-setupInternStartup(coordPID, allPIDs, [lastPID]) ->
-    lastPID!{startup, coordPID, lists:filter(fun(X) -> X /= lastPID end, allPIDs)},
-    setupInternStartup(coordPID, allPIDs, []).
-setupInternStartup(coordPID, allPIDs, [firstPID|otherPIDs]) -> 
-    lastPID!{startup, coordPID, lists:filter(fun(X) -> X /= lastPID end, allPIDs)},
-    setupInternStartup(coordPID, allPIDs, otherPIDs).
+setupInternStartup(CoordPID, AllPIDs, []) -> AllPIDs.
+setupInternStartup(CoordPID, AllPIDs, [LastPID]) ->
+    lastPID!{startup, CoordPID, lists:filter(fun(X) -> X /= LastPID end, AllPIDs)},
+    setupInternStartup(CoordPID, AllPIDs, []).
+setupInternStartup(CoordPID, AllPIDs, [FirstPID|OtherPIDs]) -> 
+    lastPID!{startup, CoordPID, lists:filter(fun(X) -> X /= FirstPID end, AllPIDs)},
+    setupInternStartup(CoordPID, AllPIDs, OtherPIDs).
 
