20d24b8c72c05a0270a28d4995a0174a1e95626b
Nowaepon <77643077+Nowaepon@users.noreply.github.com>
2024-01-16 11:56:54 +0000

    Bugfixes für Übung 11

--- tutorial/excercise_11/bully.erl
+++ tutorial/excercise_11/bully.erl
@@ -1,42 +1,40 @@
 -module(bully).
--compile().
+-compile([sendNewCoordinator/3, sendElections/2, bullyProcess/2, beginElection/3, waitForNewCoordinator/1, setup/1, setupIntern/2, setupInternStartup/3]).
 
 
 %Zum Senden der coordinator-Nachrichten an alle Prozesse außer sich selbst und den alten Coordinator
-sendNewCoordinator(NewCoordPID, OldCoordPID, []) -> ok;
+sendNewCoordinator(_, _, []) -> ok;
 sendNewCoordinator(NewCoordPID, OldCoordPID, [LastPID]) -> 
-    case
-        CurrentPID == NewCoordPID -> ok;
-        CurrentPID == OldCoordPID -> ok;
-        true -> CurrentPID!{coordinator, NewCoordPID}
-    end.
+    case LastPID of
+        NewCoordPID -> ok;
+        OldCoordPID -> ok;
+        true -> LastPID!{coordinator, NewCoordPID}
+    end;
 sendNewCoordinator(NewCoordPID, OldCoordPID, [FirstPID|OtherPIDs]) -> 
-    case
-        CurrentPID == NewCoordPID -> sendNewCoordinator(NewCoordPID, OldCoordPID, OtherPIDs);
-        CurrentPID == OldCoordPID -> sendNewCoordinator(NewCoordPID, OldCoordPID, OtherPIDs);
-        true -> CurrentPID!{coordinator, NewCoordPID}
+    case FirstPID of
+        NewCoordPID -> sendNewCoordinator(NewCoordPID, OldCoordPID, OtherPIDs);
+        OldCoordPID -> sendNewCoordinator(NewCoordPID, OldCoordPID, OtherPIDs);
+        true -> FirstPID!{coordinator, NewCoordPID}
     end.
 
 
 %Zum Senden der Election-Nachrichten an alle höheren PIDs
-sendElections(OwnerPID, []) -> ok.
+sendElections(_, []) -> ok;
 sendElections(OwnerPID, [LastPID]) -> 
     case OwnerPID < LastPID of
         true -> LastPID!{election, OwnerPID}
+    end;
 sendElections(OwnerPID, [FirstPID|OtherPIDs]) ->
     case OwnerPID < FirstPID of
         true -> FirstPID!{election, OwnerPID}, sendElections(OwnerPID, OtherPIDs);
         false -> sendElections(OwnerPID, OtherPIDs)
     end.
 
-% Zur Intitialisierung der Processes nötig
+% Zur Intitialisierung der Prozesse nötig
 bullyProcess(undefined, undefined) ->
     receive
         {startup, CoordPID, OtherPIDs} -> bullyProcess(CoordPID, OtherPIDs)
-    end.
-
-%TODO: Noch unvollständig, ABlauflogik nicht klar
-%      Noch gar nicht beachtet ist, wenn man selbst election Nachrichten erhält, während man gerade auf z. B. den rpc wartet.
+    end;
 bullyProcess(CoordPID, OtherPIDs) ->
     if 
         %Bin ich selbst der Coordinator? Dann muss ich auf rpcs antworten
@@ -44,54 +42,55 @@ bullyProcess(CoordPID, OtherPIDs) ->
             receive
                 {PID, request} -> PID!{self(), response}, bullyProcess(CoordPID, OtherPIDs)
             % TODO: Irgendwie auf election eingehen?
-            end
+            end;
         %Andernfalls überprüfe, ob der Coordinator erreichbar ist.
         %Wenn ja, alles gut, und wiederhole die Prüfung (Intervall oder so wegen Nachrichtenspam?).
         %Wenn nein, starte eine Election.
         true ->
-            CoordPID!{self(), request}
+            CoordPID!{self(), request},
             receive
-                {Pid , response} -> bullyProcess(CoordPID, OtherPIDs);
-                startElection -> beginElection(self(), CoordPID, OtherPIDs)
+                {_ , response} -> bullyProcess(CoordPID, OtherPIDs);
+                startElection -> beginElection(self(), CoordPID, OtherPIDs);
             %TODO: Müssen wir election-Nachrichten von neuen prozessen beachten und in OtherPIDs einfügen?
                 {election, SenderPid} -> 
                     SenderPid!ok,
+                    %TODO: Hier stimmt noch was nicht
                     receive
                         {coordinator, NewCoordPID} -> beginElection(self(), CoordPID, OtherPIDs)
                     end
-            after 250
-                %Starte Election
-                beginElection(self(), CoordPID, OtherPIDs)
+                after 250 ->
+                    %Starte Election
+                    beginElection(self(), CoordPID, OtherPIDs)
 
-                % sendElections(self(), OtherPIDs)
+                    % sendElections(self(), OtherPIDs)
 
-                % receive
-                %     ok -> waitForNewCoordinator(OtherPIDs)
-                % after 250
-                %     %Dann bin ich selbst coordinator
-                %     sendNewCoordinator(self(), CoordPID, OtherPIDs);
-                % end
+                    % receive
+                    %     ok -> waitForNewCoordinator(OtherPIDs)
+                    % after 250
+                    %     %Dann bin ich selbst coordinator
+                    %     sendNewCoordinator(self(), CoordPID, OtherPIDs);
+                    % end
             end
     end.
 
 %Beginnt damit, eine Election auszuführen
 beginElection(OwnPID, OldCoordPID, OtherPIDs) ->
-    sendElections(OwnPID, OtherPIDs)
+    sendElections(OwnPID, OtherPIDs),
 
     receive
         ok -> waitForNewCoordinator(OtherPIDs)
-    after 250
-        %Dann bin ich selbst coordinator
-        sendNewCoordinator(self(), OldCoordPID, OtherPIDs);
-    end
+        after 250 ->
+            %Dann bin ich selbst coordinator
+            sendNewCoordinator(self(), OldCoordPID, OtherPIDs)
+    end.
 
 %Damit mehrere ok-Nachrichten auch alle aufgebraucht werden und nicht in der Messagebox verbleiben
 %Wie abzufangen, wenn letztes ok nach coordinator kommt?
 waitForNewCoordinator(OtherPIDs) ->
     receive
-        {coordinator, NewCoordPID} -> bullyProcess(NewCoordPID, OtherPIDs),
-        ok -> waitForNewCoordinator(OtherPIDs);
-    .
+        {coordinator, NewCoordPID} -> bullyProcess(NewCoordPID, OtherPIDs);
+        ok -> waitForNewCoordinator(OtherPIDs)
+    end.
 
             %if
             %    rpc:rpc(CoordPID, request) == response -> bullyProcess(CoordPID, OtherPIDs)
@@ -100,25 +99,25 @@ waitForNewCoordinator(OtherPIDs) ->
             %receive
             %    election -> ;
             %    coordinator -> ;
-    .
+%    .
 
 %Aufgabenteil b)
 %Beliebige Menge an Prozessen startbar, Liste aller Prozesse wird zurückgegeben
-setup(Amount) -> setupIntern(Amount, [])
+setup(Amount) -> setupIntern(Amount, []).
 
 %Erstelle erst alle Prozesse, bevor ihre Logik selbst startet
 %setupIntern(0, []) -> done.
-setupIntern(0, ListPIDs) -> setupInternStartup(lists:max(ListPIDs), ListPIDs, ListPIDs).
+setupIntern(0, ListPIDs) -> setupInternStartup(lists:max(ListPIDs), ListPIDs, ListPIDs);
 setupIntern(Amount, ListPIDs) ->
     A = spawn(?MODULE, bullyProcess, [undefined, undefined]),
     setupIntern(Amount - 1, lists:append(ListPIDs, [A]).
 
 %Starte die Logik aller Prozesse, indem ihnen der Initial-Koordinator und die anderen Prozesse bekannt gemacht werden.
 %setupInternStartup(coordPID, allPIDs, []) -> darfNichtVorkommen.
-setupInternStartup(CoordPID, AllPIDs, []) -> AllPIDs.
+setupInternStartup(_, AllPIDs, []) -> AllPIDs;
 setupInternStartup(CoordPID, AllPIDs, [LastPID]) ->
     lastPID!{startup, CoordPID, lists:filter(fun(X) -> X /= LastPID end, AllPIDs)},
-    setupInternStartup(CoordPID, AllPIDs, []).
+    setupInternStartup(CoordPID, AllPIDs, []);
 setupInternStartup(CoordPID, AllPIDs, [FirstPID|OtherPIDs]) -> 
     lastPID!{startup, CoordPID, lists:filter(fun(X) -> X /= FirstPID end, AllPIDs)},
     setupInternStartup(CoordPID, AllPIDs, OtherPIDs).
